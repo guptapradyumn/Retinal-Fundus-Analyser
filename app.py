1c7d5ba43d4e74cbe6ab3afd370a6049bf9f9bf6
guptapradyumn <guptapradyumn284@gmail.com>
2019-12-12 13:18:46 +0530

    ui done 70%

--- app.py
+++ app.py
@@ -12,6 +12,11 @@ def dashboard():
 	return render_template('dashboard.html')
 
 
+@app.route('/patient') 
+def patient(): 
+	return render_template('patient.html')
+
+
 
 if __name__ == '__main__': 
 	app.run(debug = True) 
