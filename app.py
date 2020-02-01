bd547ea2aba26bb55ca71030049abcde798ae001
guptapradyumn <guptapradyumn284@gmail.com>
2020-02-01 12:46:06 +0000

    UI

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask, redirect, url_for, request ,render_template
-
+from flask_mysqldb import MySQL
 
 app = Flask(__name__) 
+
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']=''
+app.config['MYSQL_DB']='ianalyser'
+
+mysql= MySQL(app)
+
 
 @app.route('/') 
 def home(): 
@@ -13,6 +21,10 @@ def createAccount():
 
 @app.route('/home') 
 def dashboard(): 
+	cur= mysql.connection.cursor()
+	cur.execute("select * from hospital")
+	data=cur.fetchall()
+	print(data)
 	return render_template('dashboard.html')
 
 
