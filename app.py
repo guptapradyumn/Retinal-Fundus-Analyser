36c3d8a3e08d5922ea2c40a784e5079885e4c4f8
akshaym-3255 <2016.akshay.mohite@ves.ac.in>
2020-02-12 13:09:44 +0530

    demo

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'
-app.config['MYSQL_PASSWORD']=''
+app.config['MYSQL_PASSWORD']='12345'
 app.config['MYSQL_DB']='ianalyser'
 
 mysql= MySQL(app)
